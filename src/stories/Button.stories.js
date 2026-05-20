162710025db54c9c11490844c700fd6aad07e4d9
tu.nguyenc <tu.nguyenc@titancorpvn.com>
2026-05-20 10:36:29 +0700

    Initial Storybook setup

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,20 @@
+export default {
+  title: 'Components/Button',
+};
+
+export const Primary = () => {
+  return `
+    <button
+      style="
+        background:#5B5FEF;
+        color:white;
+        padding:12px 20px;
+        border:none;
+        border-radius:8px;
+        font-size:16px;
+      "
+    >
+      My First Button
+    </button>
+  `;
+};
