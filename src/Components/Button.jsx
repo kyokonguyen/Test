54d89ca63d12ff86de5c420035e25df66fbfd1d1
tu.nguyenc <tu.nguyenc@titancorpvn.com>
2026-05-20 15:53:39 +0700

    Fix storybook files

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,16 +1,76 @@
-export default function Button({ label, primary }) {
-  return (
-    <button
-      style={{
-        background: primary ? '#1EA7FD' : '#EEE',
-        color: primary ? 'white' : 'black',
-        border: 'none',
-        padding: '12px 20px',
-        borderRadius: '8px',
-        cursor: 'pointer',
-      }}
-    >
-      {label}
-    </button>
-  );
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./stories/**/*.{js,jsx,ts,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        indianred: {
+          100: "#d04668",
+          200: "#bd405f",
+        },
+
+        white: "#fff",
+
+        gray: {
+          100: "#8a8d93",
+          200: "#7c7f84",
+        },
+
+        crimson: {
+          100: "#ff4c51",
+          200: "#e64449",
+        },
+
+        orange: "#ffb400",
+
+        goldenrod: "#e6a200",
+
+        royalblue: {
+          100: "#4d82ff",
+          200: "#4676e8",
+        },
+
+        forestgreen: {
+          100: "#008926",
+          200: "#007d23",
+        },
+      },
+
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+
+      borderRadius: {
+        "num-8": "8px",
+        "num-6": "6px",
+        "num-4": "4px",
+      },
+
+      padding: {
+        "num-22": "22px",
+        "num-18": "18px",
+        "num-14": "14px",
+        "num-9": "9px",
+      },
+
+      fontSize: {
+        "num-17": "17px",
+        "num-15": "15px",
+        "num-13": "13px",
+      },
+
+      lineHeight: {
+        "num-26": "26px",
+      },
+    },
+  },
+
+  corePlugins: {
+    preflight: false,
+  },
+};
--- src/Components/Button.stories.js
+++ src/Components/Button.stories.js
@@ -3,12 +3,33 @@ import Button from './Button';
 export default {
   title: 'Components/Button',
   component: Button,
+
+  argTypes: {
+    label: {
+      control: 'text',
+    },
+
+    primary: {
+      control: 'boolean',
+    },
+
+    backgroundColor: {
+      control: 'color',
+    },
+
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large'],
+    },
+  },
 };
 
 export const Primary = {
   args: {
     label: 'Primary Button',
     primary: true,
+    backgroundColor: '#1EA7FD',
+    size: 'medium',
   },
 };
 
@@ -16,5 +37,7 @@ export const Secondary = {
   args: {
     label: 'Secondary Button',
     primary: false,
+    backgroundColor: '#EEEEEE',
+    size: 'medium',
   },
 };
